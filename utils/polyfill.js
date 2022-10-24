a3f3e3fe3e142fd4010e20b5966727eee78e9a74
thommorais <thommorais@gmail.com>
2022-10-24 11:43:59 -0300

    fix issue with server rendering

--- utils/polyfill.js
+++ utils/polyfill.js
@@ -1,21 +1,23 @@
 
+if (typeof window !== "undefined") {
 
-window.requestIdleCallback =
-    window.requestIdleCallback ||
-    function (cb) {
-        var start = Date.now();
-        return setTimeout(function () {
-            cb({
-                didTimeout: false,
-                timeRemaining: function () {
-                    return Math.max(0, 50 - (Date.now() - start));
-                },
-            });
-        }, 1);
-    };
+    window.requestIdleCallback =
+        window.requestIdleCallback ||
+        function (cb) {
+            var start = Date.now();
+            return setTimeout(function () {
+                cb({
+                    didTimeout: false,
+                    timeRemaining: function () {
+                        return Math.max(0, 50 - (Date.now() - start));
+                    },
+                });
+            }, 1);
+        };
 
-window.cancelIdleCallback =
-    window.cancelIdleCallback ||
-    function (id) {
-        clearTimeout(id);
-    };
+    window.cancelIdleCallback =
+        window.cancelIdleCallback ||
+        function (id) {
+            clearTimeout(id);
+        }
+}
